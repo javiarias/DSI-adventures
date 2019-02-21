49801b81f433c02a11c7853cadc76e236b90c4e7
javiarias <javari02@ucm.es>
2019-02-21 16:57:43 +0100

    Lab de nervios

---
+++ Prueba-21FEB/Prueba-21FEB/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Prueba_21FEB
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+
+        private void textChanged(object sender, TextChangedEventArgs e)
+        {
+            if(cityInput.Text == "")
+                cityText.Text = "";
+            else
+                cityText.Text = "En " + cityInput.Text;
+        }
+
+        private void changeMap(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MapPage), cityInput.Text);
+        }
+
+        private void makeVisible(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+                inputButton.Visibility = Visibility.Visible;
+        }
+    }
+}
